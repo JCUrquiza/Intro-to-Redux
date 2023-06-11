72a83b7640f8dc3fc13ba04729986745a8734771
Juan <gurquiza8@gmail.com>
2023-06-11 15:31:19 -0500

    Adding function to decremenet and increment by number

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { useDispatch, useSelector } from 'react-redux';
-import { increment } from './store/slices/counter';
+import { decrement, increment, incrementByAmount } from './store/slices/counter';
 import './App.css';
 
 function App() {
@@ -11,8 +11,15 @@ function App() {
     <>
       <h1>Vite + React</h1>
       <div className="card">
+        <p>count is { counter }</p>
         <button onClick={ () => dispatch( increment() ) }>
-          count is { counter }
+          Increment
+        </button>
+        <button onClick={ () => dispatch( decrement() ) }>
+          Decrement
+        </button>
+        <button onClick={ () => dispatch( incrementByAmount(2) ) }>
+          Increment by 2
         </button>
       </div>
     </>
--- src/store/slices/counter/counterSlice.js
+++ src/store/slices/counter/counterSlice.js
@@ -14,17 +14,17 @@ export const counterSlice = createSlice({
             // state.value += 1
             state.counter += 1;
         },
-        // decrement: (state) => {
-        //     state.value -= 1
-        // },
-        // incrementByAmount: (state, action) => {
-        //     state.value += action.payload
-        // },
+        decrement: (state) => {
+            state.counter -= 1
+        },
+        incrementByAmount: (state, action) => {
+            state.counter += action.payload
+        },
     },
 })
 
 // Action creators are generated for each case reducer function
-export const { increment } = counterSlice.actions
+export const { increment, decrement, incrementByAmount } = counterSlice.actions
 
 // export default counterSlice.reducer
 
