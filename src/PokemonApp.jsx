07bab0b97f5b143e5a1d621adc6d9667afad9509
Juan <gurquiza8@gmail.com>
2023-06-24 15:47:03 -0500

    Use redux to pokemon application for pagination

--- src/PokemonApp.jsx
+++ src/PokemonApp.jsx
@@ -1,26 +1,38 @@
 import { useEffect } from 'react';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { getPokemons } from './store/slices/pokemon';
+import { useState } from 'react';
 
 export const PokemonApp = () => {
-
+    
     const dispatch = useDispatch();
+    // Acceder al store de redux:
+    const { isLoading, pokemons, page } = useSelector(state => state.pokemons);
 
     useEffect(() => {
         dispatch( getPokemons() );
-    }, [])
-    
+    }, []);
 
     return (
         <>
             <h1>Pokemon</h1>
             <hr />
+            <span>Loading: { isLoading ? 'True' : 'False' }</span>
 
             <ul>
-                <li>H</li>
-                <li>H</li>
-                <li>H</li>
+                {
+                    pokemons.map( ({ name }) => (
+                        <li key={name} >{ name }</li>
+                    ))
+                }
             </ul>
+
+            <button
+                disabled={ isLoading }
+                onClick={ () => dispatch( getPokemons( page ) ) } >
+                Next
+            </button>
+
         </>
     )
 }
